f67237799e1d9eafaaac6bb405a7a7963dc2aeb5
Naoki Yokoyama <nyokoyama@theaiinstitute.com>
2023-07-07 13:23:31 -0400

    adding config to experiments dir, modified eval_llm_policy.sh to use this config

--- scripts/eval_llm_policy.sh
+++ scripts/eval_llm_policy.sh
@@ -2,13 +2,14 @@
 # Copyright [2023] Boston Dynamics AI Institute, Inc.
 
 python -um zsos.run \
-  --config-name=objectnav/ddppo_objectnav_hm3d.yaml \
+  --config-name=experiments/llm_objectnav_hm3d.yaml \
+  --config-path ../config \
   habitat_baselines.evaluate=True \
   habitat_baselines.eval_ckpt_path_dir=dummy_policy.pth \
   habitat_baselines.load_resume_state_config=False \
   habitat_baselines.rl.policy.name=LLMPolicy \
   habitat_baselines.rl.ddppo.reset_critic=False \
   habitat.simulator.habitat_sim_v0.allow_sliding=True \
-  habitat_baselines.num_environments=1 \
-  habitat_baselines.eval.video_option='["disk"]'
-#  +habitat/task/lab_sensors@habitat.task.lab_sensors.objnav_explorer=objnav_explorer
+  `# Uncomment (only) one of the next two lines to enable/disable video generation` \
+  habitat_baselines.num_environments=1 habitat_baselines.eval.video_option='["disk"]'
+#  habitat_baselines.num_environments=1
