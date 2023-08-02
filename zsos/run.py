3f72c6c3e24b6490c9a9380635bea08972f6c3f2
Jiuguang Wang <jw@theaiinstitute.com>
2023-08-02 10:20:18 -0400

    Move habitat dependencies (#4)
    Instead of commenting out the habitat dependencies, I moved them to a
    different group such that they can be installed via a `pip install -e
    .[habitat]`. This should also fix the `import cv2` error in one of the
    tests. I didn't touch the pre-commit error.
    
    ---------
    
    Co-authored-by: Naoki Yokoyama <nyokoyama@theaiinstitute.com>
    Co-authored-by: Naoki Yokoyama <132949836+naokiyokoyamabd@users.noreply.github.com>

--- zsos/run.py
+++ zsos/run.py
@@ -1,15 +1,16 @@
-import hydra
-from habitat import get_config  # noqa: F401
+import hydra  # noqa
 from habitat.config import read_write
 from habitat.config.default import patch_config
 from habitat_baselines.run import execute_exp
 from omegaconf import DictConfig
 
-# The following imports require habitat to be installed, and will register several
-# classes and make them discoverable by Hydra. This run.py script is expected
-# to only be used when habitat is installed, thus they are hidden here instead of in an
-# __init__.py file. noqa is used to suppress the unused import warning by ruff.
-import frontier_exploration  # noqa: F401
+# The following imports require habitat to be installed, and despite not being used by
+# this script itself, will register several classes and make them discoverable by Hydra.
+# This run.py script is expected to only be used when habitat is installed, thus they
+# are hidden here instead of in an __init__.py file. noqa is used to suppress the unused
+# import and unsorted import warnings by ruff.
+import frontier_exploration  # noqa
+from habitat import get_config  # noqa
 import zsos.obs_transformers.resize  # noqa: F401
 from zsos.policy import base_policy, itm_policy, llm_policy  # noqa: F401
 
