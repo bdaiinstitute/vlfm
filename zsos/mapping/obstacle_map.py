visualizing the obstacle map on video, fixing error in radius calculation

zsos/mapping/obstacle_map.py
@@ -40,7 +40,7 @@ class ObstacleMap(BaseMap):
         self._max_height = max_height
         self._area_thresh_in_pixels = area_thresh * (self.pixels_per_meter**2)
         self.__fx = None
-        kernel_size = self.pixels_per_meter * agent_radius
+        kernel_size = self.pixels_per_meter * agent_radius * 2
         # round kernel_size to nearest odd number
         kernel_size = int(kernel_size) + (int(kernel_size) % 2 == 0)
         self._navigable_kernel = np.ones((kernel_size, kernel_size), np.uint8)

zsos/utils/img_utils.py
@@ -100,8 +100,8 @@ def resize_images(images, match_dimension="height"):
     Returns:
         List[np.ndarray]: List of resized images.
     """
-    if len(images) < 2:
-        raise ValueError("At least two images are required for resizing.")
+    if len(images) == 1:
+        return images
 
     if match_dimension == "height":
         max_height = max(img.shape[0] for img in images)