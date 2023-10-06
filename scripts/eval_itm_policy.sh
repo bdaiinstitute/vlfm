4cb3006caea73397d1043bf56c4d15a0bdc83391
Naoki Yokoyama <nyokoyama@theaiinstitute.com>
2023-10-06 06:52:56 -0400

    update eval cmd for itm policy

--- scripts/eval_itm_policy.sh
+++ scripts/eval_itm_policy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Copyright [2023] Boston Dynamics AI Institute, Inc.
 
-python -um zsos.run \
+python -um vlfm.run \
   habitat_baselines.evaluate=True \
   habitat_baselines.eval_ckpt_path_dir=dummy_policy.pth \
   habitat_baselines.load_resume_state_config=False \
-  habitat_baselines.rl.policy.name=HabitatITMPolicy \
+  habitat_baselines.rl.policy.name=HabitatITMPolicyV2 \
   habitat.task.lab_sensors.base_explorer.turn_angle=30 \
   habitat_baselines.num_environments=1 \
   habitat_baselines.eval.split=val_50 \
@@ -13,5 +13,4 @@ python -um zsos.run \
   habitat_baselines.eval.video_option='["disk"]'
 #  habitat.environment.max_episode_steps=500 \
 #  habitat_baselines.test_episode_count=5 \
-#  habitat.task.lab_sensors.base_explorer.visibility_dist=2.0 \
 #  habitat_baselines.eval.video_option='[]'
